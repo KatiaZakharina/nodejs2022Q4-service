5cca217f59b62cd22242d318717ea882ecae6319
Ekaterina Zaharina <katia.zakharina@gmail.com>
2023-02-05 15:00:25 +0300

    feat: configure env variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import 'dotenv/config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT || 4000;
+  
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(port);
 }
 bootstrap();
