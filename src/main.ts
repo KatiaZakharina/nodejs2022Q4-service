20ad7404bfa63a7feb9dc7c350b43e1a9971c1c1
Ekaterina Zaharina <katia.zakharina@gmail.com>
2023-02-06 22:30:01 +0300

    feat: update api  doc

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import { parse } from 'yaml';
 import 'dotenv/config';
+
+import { dirname, join } from 'path';
+import { readFile } from 'fs/promises';
 
 import { AppModule } from './app.module';
 
@@ -10,6 +15,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const documentPath = join(dirname(__dirname), 'doc', 'api.yaml');
+  const document = parse(await readFile(documentPath, 'utf-8'));
+
+  SwaggerModule.setup('doc', app, document);
 
   await app.listen(port).then(() => {
     console.log(`Server running on port ${port}`);
